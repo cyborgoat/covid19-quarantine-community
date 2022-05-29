08fe81b0607d6f6f9d9f28856ad1f22cf6ed1154
ghostian <junxiao.guo@outlook.com>
2022-05-29 09:23:06 +0800

    update: nginx conf

--- quarantine_community/restart.sh
+++ quarantine_community/restart.sh
@@ -4,4 +4,4 @@ fuser -k 8808/tcp
 service nginx restart
 sleep 0.5
 #nohup gunicorn quarantine_community.wsgi:application -b 0.0.0.0:8808 &
-gunicorn quarantine_community.wsgi:application -c /root/pyCharmProjects/quarantine_community/conf/gunicorn_conf.py
+gunicorn quarantine_community.wsgi:application -c /root/pyCharmProjects/quarantine_community/conf/gunicorn_conf.py --daemon
