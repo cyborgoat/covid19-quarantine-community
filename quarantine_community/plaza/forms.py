e8046ad32117eeeb939ff65165438f16e952fdf6
ghostian <junxiao.guo@outlook.com>
2022-05-30 00:28:53 +0800

    update: supply registration form

--- quarantine_community/plaza/forms.py
+++ quarantine_community/plaza/forms.py
@@ -20,12 +20,16 @@ class SpecialRequestForm(forms.ModelForm):
 class SupplyRegistrationForm(forms.ModelForm):
     class Meta:
         model = SupplyRegistration
-        fields = ('country_yard', 'building_unit', 'room_num','items')
+        fields = ('country_yard', 'building_unit', 'room_num', 'items')
 
-    widgets = {
-        'country_yard': forms.ChoiceField(choices=CountryYard.objects.all()),
-        'building_unit': forms.ChoiceField(choices=BuildingUnit.objects.all()),
-        'room_num': forms.TextInput(
-            attrs={'class': 'form-control input-lg', 'placeholder': 'Enter Room Number'}),
-        'items': forms.MultipleChoiceField(choices=SupplyItem.objects.all(), widget=forms.CheckboxSelectMultiple),
-    }
+        widgets = {
+            'country_yard': forms.Select(attrs={'class': 'form-control'}),
+            'building_unit': forms.Select(attrs={'class': 'form-control'}),
+            'room_num': forms.TextInput(
+                attrs={'class': 'form-control input-lg', 'placeholder': 'Enter Room Number'}),
+            'items': forms.CheckboxSelectMultiple(attrs={'class': ''}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(SupplyRegistrationForm, self).__init__(*args, **kwargs)
+        # self.fields['items'].label = False
