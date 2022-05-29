c14e42ef9859317cc24e487621ee5dcc8c4394bc
ghostian <junxiao.guo@outlook.com>
2022-05-30 00:39:02 +0800

    update: supply registration form

--- quarantine_community/plaza/forms.py
+++ quarantine_community/plaza/forms.py
@@ -23,13 +23,16 @@ class SupplyRegistrationForm(forms.ModelForm):
         fields = ('country_yard', 'building_unit', 'room_num', 'items')
 
         widgets = {
-            'country_yard': forms.Select(attrs={'class': 'form-control'}),
-            'building_unit': forms.Select(attrs={'class': 'form-control'}),
+            'country_yard': forms.Select(attrs={'class': 'form-select'}),
+            'building_unit': forms.Select(attrs={'class': 'form-select'}),
             'room_num': forms.TextInput(
-                attrs={'class': 'form-control input-lg', 'placeholder': 'Enter Room Number'}),
+                attrs={'class': 'form-control input-lg', 'placeholder': '请输入房间号'}),
             'items': forms.CheckboxSelectMultiple(attrs={'class': ''}),
         }
 
     def __init__(self, *args, **kwargs):
         super(SupplyRegistrationForm, self).__init__(*args, **kwargs)
-        # self.fields['items'].label = False
+        self.fields['country_yard'].label = "所属院"
+        self.fields['building_unit'].label = "所属单元"
+        self.fields['room_num'].label = "房间号"
+        self.fields['items'].label = "所需物品"
