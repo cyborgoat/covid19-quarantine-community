28c963f06d8405ddd20f8fd034251ccedcbe85dc
ghostian <junxiao.guo@outlook.com>
2022-05-29 20:54:36 +0800

    update: titlees

--- quarantine_community/plaza/admin.py
+++ quarantine_community/plaza/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from plaza.models import OfficialNotification, SpecialRequest
+from plaza.models import OfficialNotification, SpecialRequest,CountryYard,BuildingUnit,SupplyRegistration
 
 admin.site.register(OfficialNotification)
 admin.site.register(SpecialRequest)
+admin.site.register(CountryYard)
+admin.site.register(BuildingUnit)
+admin.site.register(SupplyRegistration)
--- quarantine_community/plaza/models.py
+++ quarantine_community/plaza/models.py
@@ -5,10 +5,17 @@ from overview.models import User
 
 
 class CountryYard(models.Model):
-    name = models.CharField(max_length=64)
+    yard_num = models.IntegerField(blank=True,null=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.yard_num}号院'
+
+
+class BuildingUnit(models.Model):
+    unit_num = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.unit_num}号楼'
 
 
 class OfficialNotification(models.Model):
@@ -25,8 +32,12 @@ class OfficialNotification(models.Model):
 class SpecialRequest(models.Model):
     title = models.CharField(max_length=64)
     name = models.CharField(max_length=32)
-    responder = models.ForeignKey(User,on_delete=models.CASCADE,related_name='responder')
+    responder = models.ForeignKey(User, on_delete=models.CASCADE, related_name='responder')
     body = models.TextField()
 
     def __str__(self):
         return self.title
+
+
+class SupplyRegistration(models.Model):
+    room_num = models.CharField(max_length=16)
