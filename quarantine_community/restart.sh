c833d90fbd6a1bffe58dc91ad4a7dbaffafd31a3
ghostian <junxiao.guo@outlook.com>
2022-05-30 20:48:50 +0800

    update: api

--- quarantine_community/restart.sh
+++ quarantine_community/restart.sh
@@ -5,3 +5,4 @@ service nginx restart
 sleep 0.5
 #nohup gunicorn quarantine_community.wsgi:application -b 0.0.0.0:8808 &
 gunicorn quarantine_community.wsgi:application -c /root/pyCharmProjects/quarantine_community/conf/gunicorn_conf.py --daemon
+#gunicorn quarantine_community.wsgi:application -c /root/home/cyborgoat/pyCharmProjects/quarantine_community/conf/gunicorn_conf.py --daemon
