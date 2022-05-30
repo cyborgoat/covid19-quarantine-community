fcdc5ebc28facb9effad32e44785fe9cad71f636
ghostian <junxiao.guo@outlook.com>
2022-05-30 14:19:57 +0800

    update: restapi

--- quarantine_community/offline-processor/restapi.py
+++ quarantine_community/offline-processor/restapi.py
@@ -1,28 +1,14 @@
 import datetime
 import json
+import pathlib
 
+import numpy as np
 import requests
 import pandas as pd
 from requests.auth import HTTPBasicAuth
 
-start_date = datetime.date.today()
-end_date = datetime.date.today()
-
-print(start_date,end_date)
-exit()
-
-with open('credentials.json', 'r') as f:
-    credentials = json.load(f)
-
-
-def api2df(url):
-    response = requests.get(url, auth=HTTPBasicAuth(credentials['name'], credentials['password']))
-    js = response.json()['results']
-    df = pd.DataFrame.from_dict(js)
-    if len(df) > 0:
-        df['created_on'] = (df['created_on'])
-    return df
-
+today = datetime.date.today()
+end_date = datetime.date.today() - datetime.timedelta(days=1)
 
 DEBUG = False
 api_url = "http://121.43.36.132/api/" if not DEBUG else "http://127.0.0.0:8000/api/"
@@ -30,8 +16,29 @@ api_url = "http://121.43.36.132/api/" if not DEBUG else "http://127.0.0.0:8000/a
 supply_registration_url = api_url + 'supply_registration/'
 special_request_url = api_url + 'special_requests/'
 
-supply_df = api2df(supply_registration_url)
-special_df = api2df(special_request_url)
+with open('credentials.json', 'r') as f:
+    credentials = json.load(f)
 
-print(supply_df)
-print(special_df)
+with open('word_dict.txt', 'r') as f:
+    word_dict = dict()
+    lines = f.readlines()
+    for line in lines:
+        words = line.strip().split(' ')
+        word_dict[words[0]] = words[1]
+
+
+def api2df(url, file_title: str):
+    response = requests.get(url, auth=HTTPBasicAuth(credentials['name'], credentials['password']))
+    js = response.json()['results']
+    df = pd.DataFrame.from_dict(js)
+    if len(df) > 0:
+        df['created_on'] = pd.to_datetime(df['created_on']).map(datetime.datetime.date)
+        df = df.loc[df['created_on'] == today]
+    df.columns = [word_dict[i] for i in df.columns]
+    df.to_excel(pathlib.Path('data', f'{file_title}-{today}.xlsx'))
+    return df
+
+
+if __name__ == '__main__':
+    supply_df = api2df(supply_registration_url, '必需品需求')
+    special_df = api2df(special_request_url, '特殊需求')
